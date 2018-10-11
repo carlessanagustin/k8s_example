63789580d12f7a4aef260f7e46a8fa856b1e28ad
Carles San Agustin <carles.sam@gmail.com>
2018-10-11 16:53:02 +0200

    makefile variables cleanup

--- config.mk
+++ config.mk
@@ -1,13 +1,10 @@
-HTDOCS ?= $(shell pwd)/pv-local
-SHARED_DIR ?= /tmp/k8s
-MK_SESSION ?= minikube
 POD_NAMESPACE ?= ingress-nginx
-
-# local-ssd | pd-ssd | pd-standard
-VTYPE ?= pd-standard
-VNAME ?= gke-my-data-disk
-VZONE ?= europe-west1-b
-VSIZE ?= 10GB
 
 #INVENTORY ?= ./ansible/inventory/vagrant
 INVENTORY ?= ./ansible/inventory/gcp
+
+LIMIT ?=
+TAG ?=
+DEBUG ?=
+NODE_NAME ?= k8s-test-worker-001
+GRACE_PERIOD ?= 15
